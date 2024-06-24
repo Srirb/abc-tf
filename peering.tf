b6998a3931b1ce269b46edde5a49464b51e22002
Srimdu <srirambalraj8@gmail.com>
2024-06-24 10:27:04 +0530

    c4

--- peering.tf
+++ peering.tf
@@ -1,12 +1,12 @@
-# module "peering" {
-#   source                     = "terraform-google-modules/network/google//modules/network-peering"
-#   version                    = "~> 5.0"  # Match the network module version
-#   for_each                   = var.vpc_peerings
-#   prefix                     = "peering"       # Optional prefix for peering names
-#   local_network              = module.shared_vpc_host[each].network_self_link
-#   peer_network               = module.shared_vpc_host[each.value].network_self_link
+module "peering" {
+  source                     = "terraform-google-modules/network/google//modules/network-peering"
+  version                    = "~> 5.0"  # Match the network module version
+  for_each                   = var.vpc_peerings
+  prefix                     = "peering"       # Optional prefix for peering names
+  local_network              = module.shared_vpc_host[each].network_self_link
+  peer_network               = module.shared_vpc_host[each.value].network_self_link
 
-#   # Optional: Export custom routes if needed
-#   export_local_custom_routes = true  
-#   export_peer_custom_routes  = true
-# }
+  # Optional: Export custom routes if needed
+  export_local_custom_routes = true  
+  export_peer_custom_routes  = true
+}
